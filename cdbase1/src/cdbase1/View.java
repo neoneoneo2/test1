4539f748f51dea4301751172f9b5135635918bb2
neoneoneo2 <letstalk80@bk.ru>
2011-11-25 22:57:18 +0300

    Заставка

--- cdbase1/src/cdbase1/View.java
+++ cdbase1/src/cdbase1/View.java
@@ -1,77 +1,51 @@
 package cdbase1;
 
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.Viewer;
+
+
+
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.*;
 
 public class View extends ViewPart {
-	public static final String ID = "cdbase1.view";
+	public static final String ID = "cdBase.view";
 
-	private TableViewer viewer;
-
-	/**
-	 * The content provider class is responsible for providing objects to the
-	 * view. It can wrap existing objects in adapters or simply return objects
-	 * as-is. These objects may be sensitive to the current input of the view,
-	 * or ignore it and always show the same content (like Task List, for
-	 * example).
-	 */
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-
-		public void dispose() {
-		}
-
-		public Object[] getElements(Object parent) {
-			if (parent instanceof Object[]) {
-				return (Object[]) parent;
-			}
-	        return new Object[0];
-		}
+	public void createPartControl(Composite parent) 
+	{
+		parent.setLayout(new RowLayout());		
+		Button button = new Button(parent, SWT.PUSH);
+		Button button1 = new Button(parent, SWT.PUSH);
+		Button button2 = new Button(parent, SWT.PUSH);		
+		button.setText("Push me");
+		button1.setText("Push me1");
+		button2.setText("Push me2");
+		
+		//Нажатие кнопки
+		button.addSelectionListener(
+				new SelectionAdapter() {
+		            @Override
+		            public void widgetSelected(SelectionEvent e) 
+		            {		               
+		            	e.display.getActiveShell().setVisible(false);
+		            	//parent.setVisible(false);
+		            	//System.out.println("pressed");
+		            }
+		        });
+		//button.setLayoutData(new RowData(50, 40));
+		/*
+		Image img=new Image(cdbase.View.class.getResourceAsStream("..\\icons\\cdbase_logo.png"));
+		Label lbl=new Label(parent.getDisplay(),0);
+		lbl.setImage(img);
+		*/
 	}
 
-	class ViewLabelProvider extends LabelProvider implements
-			ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().getSharedImages().getImage(
-					ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
-
-	/**
-	 * This is a callback that will allow us to create the viewer and initialize
-	 * it.
-	 */
-	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		// Provide the input to the ContentProvider
-		viewer.setInput(new String[] {"One", "Two", "Three"});
-	}
-
-	/**
-	 * Passing the focus request to the viewer's control.
-	 */
-	public void setFocus() {
-		viewer.getControl().setFocus();
+	
+	public void setFocus() 
+	{
+		//viewer.getControl().setFocus();
 	}
 }
